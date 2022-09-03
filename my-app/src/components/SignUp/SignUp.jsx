ad6f842da29ad2c8d2676dced86340a22a760742
SinaLiri <Sina.Liri@yahoo.com>
2022-09-03 19:03:26 +0430

    toastify add and validation complete

--- my-app/src/components/SignUp/SignUp.jsx
+++ my-app/src/components/SignUp/SignUp.jsx
@@ -1,5 +1,8 @@
 import React, { useState , useEffect } from 'react';
 import { validate } from '../validate';
+import { ToastContainer } from 'react-toastify';
+import { notify } from '../toast';
+  import 'react-toastify/dist/ReactToastify.css';
 const SignUp = () => {
 
     const [data, setData] = useState({
@@ -10,10 +13,13 @@ const SignUp = () => {
         isAccepted: false
     })
     const [errors,setErrors]=useState({});
+    const [touch,setTouch]=useState({});
+
+
     useEffect(()=>{
         setErrors(validate(data));
         console.log(errors)
-    },[data])
+    }, [data])
 
     const changeHandler = event => {
         if (event.target.name === "isAccepted") {
@@ -23,36 +29,66 @@ const SignUp = () => {
         }
         console.log(data)
     }
+    const touchHandler=(event)=>{
+        setTouch({ ...touch, [event.target.name]:true})
+    }
 
+    const submitHandler=(event)=>{
+        event.preventDefault();
+
+        if(!Object.keys(errors).length){
+            console.log(data)
+            notify("Signed in successfully","success")
+        }else{
+            notify("invalid data","error")
+            setTouch({
+                name:true,
+                email:true,
+                password:true,
+                confirmPassword:true,
+                isAccepted:true,
+            })
+        }
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" value={data.name} onChange={changeHandler} />
+                    <input type="text" name="name" value={data.name} onChange={changeHandler} onFocus={touchHandler} />
+                    {errors.name && touch.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="text" name="email" value={data.email} onChange={changeHandler} />
+                    <input type="text" name="email" value={data.email} onChange={changeHandler} onFocus={touchHandler}  />
+                    {errors.email && touch.email && <span>{errors.email}</span>}
+
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" value={data.password} onChange={changeHandler} />
+                    <input type="password" name="password" value={data.password} onChange={changeHandler} onFocus={touchHandler}  />
+                    {errors.password && touch.password && <span>{errors.password}</span>}
+
                 </div>
                 <div>
                     <label>Confirm Password</label>
-                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} />
+                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} onFocus={touchHandler}  />
+                    {errors.confirmPassword && touch.confirmPassword && <span>{errors.confirmPassword}</span>}
+
                 </div>
                 <div>
                     <label>I accet terms of privacy policy</label>
-                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} />
+                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} onFocus={touchHandler}  />
+                    {errors.isAccepted && touch.isAccepted && <span>{errors.isAccepted}</span>}
+
                 </div>
                 <div>
                     <a href="#">Login</a>
                     <button type="submit">Sign Up</button>
                 </div>
             </form>
+            <ToastContainer/>
         </div>
     );
 };
