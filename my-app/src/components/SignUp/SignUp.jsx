51fcd3e72cc640cf9de436f32a7ac7c2c250e217
SinaLiri <Sina.Liri@yahoo.com>
2022-09-05 00:31:20 +0430

    sign up form complete

--- my-app/src/components/SignUp/SignUp.jsx
+++ my-app/src/components/SignUp/SignUp.jsx
@@ -55,38 +55,68 @@ const SignUp = () => {
     return (
         <div className={styles.container}>
         <div className={styles.imgwrapper}>
-            <img src={imgsignup}/>
+            <img src={imgsignup} alt="sign up"/>
         </div>
         <div className={styles.formContainer}>
             <form onSubmit={submitHandler} className={styles.form}>
                 <h2>Sign Up</h2>
                 <div className={styles.inputwrapper}>
                     <label>Name</label>
-                    <input type="text" name="name" value={data.name} onChange={changeHandler} onFocus={touchHandler} />
+                    <input
+                     className={(errors.name && touch.name ) && styles.uncompleted}
+                     type="text"
+                     name="name"
+                     value={data.name} 
+                     onChange={changeHandler} 
+                     onFocus={touchHandler} />
                     {errors.name && touch.name && <span>{errors.name}</span>}
                 </div>
                 <div className={styles.inputwrapper}>
                     <label>Email</label>
-                    <input type="text" name="email" value={data.email} onChange={changeHandler} onFocus={touchHandler}  />
+                    <input 
+                    className={(errors.email && touch.email ) && styles.uncompleted }
+                    type="text" 
+                    name="email" 
+                    value={data.email} 
+                    onChange={changeHandler} 
+                    onFocus={touchHandler}  />
                     {errors.email && touch.email && <span>{errors.email}</span>}
 
                 </div>
                 <div className={styles.inputwrapper}>
                     <label>Password</label>
-                    <input type="password" name="password" value={data.password} onChange={changeHandler} onFocus={touchHandler}  />
+                    <input 
+                    className={(errors.password && touch.password ) && styles.uncompleted}
+                    type="password" 
+                    name="password" 
+                    value={data.password} 
+                    onChange={changeHandler} 
+                    onFocus={touchHandler}  />  
                     {errors.password && touch.password && <span>{errors.password}</span>}
 
                 </div>
                 <div className={styles.inputwrapper}>
                     <label>Confirm Password</label>
-                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} onFocus={touchHandler}  />
+                    <input 
+                    className={(errors.confirmPassword && touch.confirmPassword )&& styles.uncompleted}
+                    type="password" 
+                    name="confirmPassword" 
+                    value={data.confirmPassword} 
+                    onChange={changeHandler} 
+                    onFocus={touchHandler}  />
                     {errors.confirmPassword && touch.confirmPassword && <span>{errors.confirmPassword}</span>}
 
                 </div>
                 <div className={styles.inputwrapper}>
                     <div className={styles.flex}>
                     <label>I accet terms of privacy policy</label>
-                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} onFocus={touchHandler}  />
+                    <input 
+                    className={(errors.isAccepted && touch.isAccepted )? styles.uncompleted : null}
+                    type="checkbox" 
+                    name="isAccepted" 
+                    value={data.isAccepted} 
+                    onChange={changeHandler} 
+                    onFocus={touchHandler}  />
                     </div>
                     {errors.isAccepted && touch.isAccepted && <span>{errors.isAccepted}</span>}
 
